9b0c8565d2a049e5db2509a413b45cdde57f51ce
rems2rems <remy.brousset@gmail.com>
2015-08-28 19:20:14 +0200

    bugfix on lights values

--- javascript/romanScale.js
+++ javascript/romanScale.js
@@ -23,16 +23,22 @@
       while (!(light1 - light2 < 10)) {
         motor.forward();
         nbSteps += 1;
+        light1 = photoDiode1.getValue();
+        light2 = photoDiode2.getValue();
       }
     }
     if (light2 > light1) {
       while (!(light1 - light2 > 100)) {
         motor.backward();
         nbSteps -= 1;
+        light1 = photoDiode1.getValue();
+        light2 = photoDiode2.getValue();
       }
       while (!(light1 - light2 < 10)) {
         motor.forward();
         nbSteps += 1;
+        light1 = photoDiode1.getValue();
+        light2 = photoDiode2.getValue();
       }
     }
     if (light2 - light1 < 10) {
