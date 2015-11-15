11a210c355e13d247d877d62a87a10dafe247d1b
rems2rems <remy.brousset@gmail.com>
2015-11-15 17:03:37 +0100

    added reboot without internet connection

--- coffeescript/plan_reboot.coffee
+++ coffeescript/plan_reboot.coffee
@@ -18,10 +18,16 @@ db.get config.stand_id
     
     if stand.sleepMode is on and device.planWakeup and device.shutdown
         device.planWakeup(stand.sleepDuration)
-        console.log "system will reboot 10 seconds after shutdown"
+        console.log "system will reboot " + stand.sleepDuration + " seconds after shutdown"
         device.shutdown()
         console.log "system is going to shutdown..."
         
 .catch (err)->
 
     console.log err 
+
+    device = require './devices/arietta_g25'
+    device.planWakeup(3300)
+    console.log "system will reboot 3300 seconds after shutdown"
+    device.shutdown()
+    console.log "system is going to shutdown..."
--- javascript/plan_reboot.js
+++ javascript/plan_reboot.js
@@ -13,12 +13,18 @@
     device = require('./devices/' + stand.device);
     if (stand.sleepMode === true && device.planWakeup && device.shutdown) {
       device.planWakeup(stand.sleepDuration);
-      console.log("system will reboot 10 seconds after shutdown");
+      console.log("system will reboot " + stand.sleepDuration + " seconds after shutdown");
       device.shutdown();
       return console.log("system is going to shutdown...");
     }
   })["catch"](function(err) {
-    return console.log(err);
+    var device;
+    console.log(err);
+    device = require('./devices/arietta_g25');
+    device.planWakeup(3300);
+    console.log("system will reboot 3300 seconds after shutdown");
+    device.shutdown();
+    return console.log("system is going to shutdown...");
   });
 
 }).call(this);
