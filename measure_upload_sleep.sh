e1815da02f558356fd393b27c786afb07a65aa97
rems2rems <remy.brousset@gmail.com>
2016-01-09 19:14:12 +0100

    removed log redirections

--- measure_upload_sleep.sh
+++ measure_upload_sleep.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+#script a lancer dans le cron avec : > /root/mesure.log 2>&1
+
 ./setdate_from_google
 
 node /root/openbeelab-embedded-uploading/javascript/main.js
 
-echo chemin wakeup_ena `which wakeup_ena` > /root/plan_reboot.log 2>&1
+echo chemin wakeup_ena `which wakeup_ena`
 if [ ! -f /sys/class/rtc/rtc0/wakealarm ]; then
-    echo "le chemin vers le fichier wakealarm n'existe pas!" >> /root/plan_reboot.log 2>&1
+    echo "le chemin vers le fichier wakealarm n'existe pas!"
 fi
 
-node /root/openbeelab-embedded-uploading/javascript/plan_reboot.js >> /root/plan_reboot.log 2>&1
+node /root/openbeelab-embedded-uploading/javascript/plan_reboot.js
